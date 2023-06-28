87137e354acaf599c8b0eeb69fe19438c0c95622
d3nnyyy <d3nnn41k@gmail.com>
2023-06-28 19:49:41 +0300

    Initial commit

---
+++ src/main/java/ua/tsebulia/AuthorizationAndAuthentication/AuthorizationAndAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package ua.tsebulia.AuthorizationAndAuthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationAndAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorizationAndAuthenticationApplication.class, args);
+	}
+
+}
