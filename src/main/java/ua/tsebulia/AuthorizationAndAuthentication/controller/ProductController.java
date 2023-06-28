f269879a22deb82df2fdd9ecce1845bdc5d09e68
d3nnyyy <d3nnn41k@gmail.com>
2023-06-28 20:32:04 +0300

    set restricts for endpoints

--- src/main/java/ua/tsebulia/AuthorizationAndAuthentication/controller/ProductController.java
+++ src/main/java/ua/tsebulia/AuthorizationAndAuthentication/controller/ProductController.java
@@ -1,6 +1,7 @@
 package ua.tsebulia.AuthorizationAndAuthentication.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,10 +28,12 @@ public class ProductController {
     }
 
     @GetMapping("/all")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public List<Product> getAllProducts() {
         return productService.getAllProducts();
     }
 
+    @PreAuthorize("hasAuthority('ROLE_USER')")
     @GetMapping("{id}")
     public Product getProductById(@PathVariable int id) {
         return productService.getProductById(id);
